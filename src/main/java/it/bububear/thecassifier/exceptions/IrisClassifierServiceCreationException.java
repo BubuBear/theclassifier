18205a9f411ddf2f9cab3e79c82eef22a4121930
BubuBear <s.nepop@hotmail.com>
2021-04-19 18:11:52 +0200

    FEAT: ADD IrisClassifierServiceWeka WHICH IS RESPONSIBLE TO CLASSIFY IRIS
    - ADD IrisClassifierService which define classify method which is responsible to interact with whatever ML or statistics classification methods
    - ADD IrisClassifierServiceWeka which is responsible for speak with iris weka classifier
    - ADD IrisClassifyException and IrisClassifierServiceCreationException which are suppose to handle with weka internal exceptions
    - REMOVE IrisClassifierServiceSimple.java which was introduce only for stub purpose

---
+++ src/main/java/it/bububear/thecassifier/exceptions/IrisClassifierServiceCreationException.java
@@ -0,0 +1,7 @@
+package it.bububear.thecassifier.exceptions;
+
+public class IrisClassifierServiceCreationException extends Exception {
+  public IrisClassifierServiceCreationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
