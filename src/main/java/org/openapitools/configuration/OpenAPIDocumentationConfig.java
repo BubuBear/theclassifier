2923fba3e60806bf025099a30f615bbae237a397
BubuBear <s.nepop@hotmail.com>
2021-04-23 00:28:38 +0200

    FIX: WRONG API PACKAGE NAME INSIDE Docket CREATION METHOD
    The problem was unseen because the page of actual swagger was not visited after first code generation wit openapigenerator

--- src/main/java/org/openapitools/configuration/OpenAPIDocumentationConfig.java
+++ src/main/java/org/openapitools/configuration/OpenAPIDocumentationConfig.java
@@ -38,7 +38,7 @@ public class OpenAPIDocumentationConfig {
     public Docket customImplementation(ServletContext servletContext, @Value("${openapi.irisClassifier.base-path:}") String basePath) {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                    .apis(RequestHandlerSelectors.basePackage("it.openapigenerator.theclassifier.apis"))
+                    .apis(RequestHandlerSelectors.basePackage("it.bububear.thecassifier.controllers"))
                     .build()
                 .pathProvider(new BasePathAwareRelativePathProvider(servletContext, basePath))
                 .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
